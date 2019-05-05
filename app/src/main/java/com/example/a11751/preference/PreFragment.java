c442d75b3a97dd0d239d4754cbe13c90aacfe285
cj000111 <1175184380@qq.com>
2019-05-05 12:02:42 +0800

    first commit

---
+++ app/src/main/java/com/example/a11751/preference/PreFragment.java
@@ -0,0 +1,26 @@
+package com.example.a11751.preference;
+
+
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
+import android.widget.TextView;
+import com.example.a11751.preference.R;
+
+public class PreFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+
+        //从xml文件加载选项
+        addPreferencesFromResource(R.xml.preferences);
+        //修改运行后生成的文件名
+        getPreferenceManager().setSharedPreferencesName("mysetting");
+    }
+
+
+}
